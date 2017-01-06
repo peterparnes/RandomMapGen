139d9c299b33efe74f2d9e58464524730c7e7131
Peter Parnes <peter@parnes.com>
2017-01-06 13:28:28 +0100

    After ch 1

---
+++ Assets/RandomMapGen/Scripts/RandomMapTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMapTester : MonoBehaviour {
+
+	[Header("Map Dimensions")]
+	public int mapWidth = 20;
+	public int mapHeight = 20;
+
+	public Map map; 
+
+	// Use this for initialization
+	void Start () {
+		map = new Map ();
+		
+	}
+	
+	public void MakeMap() {
+		map.NewMap (mapWidth, mapHeight);
+		Debug.Log ("Created a new map: " + map.columns + " x " + map.rows + " map");
+	}
+}
---
+++ Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Sides { 
+	Bottom, 
+	Right, 
+	Left,
+	Top
+}
+
+public class Tile {
+
+	public int id = 0;
+	public Tile[] neighbours = new Tile[4]; 
+
+
+}
